e6d038f2e7bab611f5529de84845e7485dc21521
CosWeLL <coswell23@gmail.com>
2019-04-23 06:08:11 +0300

    fix "Exercise 2.40" v1.1

--- src/Chapter 2/Exercise 2.40.scm
+++ src/Chapter 2/Exercise 2.40.scm
@@ -45,7 +45,11 @@
 (define (prime-sum? pair)
   (prime? (+ (car pair) (cadr pair))))
 
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
 (define (prime-sum-pairs n)
-  (filter prime-sum?
-          (uniq-pairs n)))
+  (map make-pair-sum
+       (filter prime-sum?
+               (uniq-pairs n))))
 
